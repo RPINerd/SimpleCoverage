320c35dca9021ad95660a0859b6e9e50d659724a
Chris <Rpinerd@gmail.com>
2024-11-20 15:20:41 -0500

    Initial build commit

---
+++ sc_class.py
@@ -0,0 +1,48 @@
+from Bio.SeqRecord import SeqRecord
+
+
+class Match:
+    """
+    Object to store information about a match between a seq and target sequence
+    """
+
+    def __init__(self, seq_id: str, start: int, end: int, mismatches: list[int] = []) -> None:
+        self.seq_id = seq_id
+        self.start = start
+        self.end = end
+        self.mismatches = mismatches
+
+
+class Target:
+    """
+    Object to store information about the target sequence including matches to it and the positions they cover
+    """
+
+    def __init__(self, record: SeqRecord) -> None:
+        self.id = record.id
+        self.name = record.name
+        self.seq = record.seq
+        self.length = len(record.seq)
+        self.coverage_map: list[int] = [0] * len(record.seq)
+        self.matches: list[Match] = []
+
+    def add_match(self, match: Match) -> None:
+
+        for i in range(match.start, match.end):
+            self.coverage_map[i] += 1
+        self.matches.append(match)
+
+    def print_coverage(self) -> None:
+        bp_with_coverage = 0
+        for i in self.coverage_map:
+            if i > 0:
+                bp_with_coverage += 1
+        bp_pct = bp_with_coverage / self.length
+        print(f"""Total coverage for {self.id} over {self.length} bp: {bp_pct:.2%} ({bp_with_coverage} bp)\n""")
+
+    def print_coverage_map(self, columns: int) -> None:
+        print(f"Coverage map for target {self.id}:")
+        for i in range(0, len(self.coverage_map), columns):
+            col_end = i + columns
+            print("".join([str(i) for i in self.coverage_map[i:col_end]]))
+            print("".join([str(i) for i in self.seq[i:col_end]]) + "\n")
---
+++ scrun.py
@@ -0,0 +1,128 @@
+"""
+    Simple Coverage Tool | RPINerd, 11/20/24
+
+    Given a list of input seqs (Primers, input, whatever) and a set of
+    target sequences, this tool will calculate the coverage
+
+    Allows for a threshold option to be set for allowed mismatches between
+    the input and target sequence
+"""
+
+import argparse
+import os
+import sys
+
+from Bio import SeqIO
+from Bio.Align import PairwiseAligner
+from Bio.SeqRecord import SeqRecord
+
+from sc_class import Match, Target
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Basic argument parser for the script
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, help="Path to the input fasta file")
+    target_type = parser.add_mutually_exclusive_group(required=True)
+    target_type.add_argument("-t", "--targets", type=str, help="Path to the target sequences fasta file")
+    target_type.add_argument(
+        "-a", "--accession", type=str, help="!NOT IMPLEMENTED! Accession ID of the target sequence"
+    )
+    parser.add_argument(
+        "--columns", type=int, required=False, default=80, help="Number of columns to print coverage in"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=False,
+        default="osm_metrics",
+        help="Prefix of the output files for the coverage results",
+    )
+    parser.add_argument(
+        "-m",
+        "--mismatches",
+        type=int,
+        required=False,
+        default=8,
+        help="Number of allowed mismatches between seq and target",
+    )
+
+    return parser.parse_args()
+
+
+def load_fasta(fasta_file: str) -> list[SeqRecord]:
+    """
+    Load a fasta file and return the SeqRecord objects
+    """
+    records: list[SeqRecord] = []
+    with open(fasta_file, "r") as f:
+        for record in SeqIO.parse(f, "fasta"):
+            records.append(record)
+
+    return records
+
+
+def main(args: argparse.Namespace) -> None:
+    """"""
+
+    # Load the input and target sequences
+    input = load_fasta(args.input)
+    targets = [Target(record) for record in load_fasta(args.targets)]  # - This is kind of silly to do, maybe rework
+
+    # Calculate coverage of the seq pool
+    aligner: PairwiseAligner = PairwiseAligner()
+    aligner.mode = "local"
+    aligner.open_gap_score = -100
+    aligner.target_internal_open_gap_score = -100
+    for target in targets:
+        print(f"Aligning input to {target.id}...")
+        for index, seq in enumerate(input):
+            print(f"\tAligning {seq.id} ({index + 1}/{len(input)})", end="\r")
+            # TODO future func, add a function to calculate mismatches between seq and target
+            alignments = aligner.align(seq.seq, target.seq)
+            for alignment in alignments:
+                # Match/mismatch is +1/-1, so we can use the score to calculate the number of mismatches
+                if alignment.score > (len(seq.seq) - args.mismatches):
+                    # print(alignment.score)
+                    # print(alignment)
+                    # print(f"aligned:\n{alignment.aligned}\n")
+                    # print(f"indices:\n{alignment.indices}\n")
+                    # print(f"sequences:\n{alignment.sequences}\n")
+                    # print(f"counts():\n{alignment.counts()}\n")
+                    # print(f"coordinates:\n{alignment.coordinates}\n")
+                    # print(f"shape:\n{alignment.shape}\n")
+                    # print(f"substitutions:\n{alignment.substitutions}\n")
+                    start = alignment.coordinates[1][0]
+                    end = alignment.coordinates[1][1]
+                    hit = Match(seq.id, start, end)
+                    target.add_match(hit)
+        print(" " * 80, end="\r")
+        print("Done!")
+    for target in targets:
+        target.print_coverage()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # Check to make sure files provided exist
+    files_missing = []
+    if args.input and not os.path.exists(args.input):
+        files_missing.append(f"Input fasta file {args.input} does not exist!")
+    if args.targets and not os.path.exists(args.targets):
+        files_missing.append(f"Target fasta file {args.targets} does not exist!")
+    if len(files_missing) > 0:
+        print("\n".join(files_missing))
+        sys.exit(1)
+
+    # TODO future func, check if accession or catalog is valid
+    if args.accession:
+        raise NotImplementedError("Accession ID checking not implemented yet")
+
+    if os.path.exists(args.output):
+        print(f"Output file {args.output} already exists.. will overwrite.")
+
+    main(args)
